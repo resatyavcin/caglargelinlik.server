71f33003f5a6f3767af29144f6ec39b3f8495695
resat <resatyavcin@icloud.com>
2023-11-23 00:05:43 +0300

    Add procfile for custom start script

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+async function databaseConnection() {
+  try {
+    await mongoose.connect(process.env.DB_STRING);
+    console.log("MongoDB'ye başarı ile bağlanılamadı");
+  } catch (error) {
+    console.log("MongoDB'ye Bağlanılamadı");
+  }
+}
+
+app.get('/', (req, res) => {
+  res.send('Health Check Done');
+});
+
+app.listen(PORT, async function () {
+  await databaseConnection();
+  console.log(`Server ${PORT} üzerinde dinleniyor...`);
+});
