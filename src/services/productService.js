fc80a89c192c6ec2cb8d3666305b7388e7b4404e
resat <resatyavcin@icloud.com>
2023-11-28 18:11:47 +0300

    Complete Product Feature

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,34 +1,24 @@
 const ProductModel = require('../models/Product');
+const { findByIdAndDelete } = require('../models/User');
 
 module.exports = {
-  create: async function ({
-    code,
-    name,
-    quantity,
-    isSecondHand,
-    isSold,
-    isActive,
-  }) {
+  create: async function ({ code, name, isSecondHand, isSold, isActive }) {
     const product = new ProductModel({
       code,
       name,
-      quantity,
       isSecondHand,
       isSold,
       isActive,
     });
 
-    // if (isExist) {
-    //   throw new Error('Ürün stoklarda mevcut. Tekrar oluşturulamaz.');
-    //   return;
-    // }
-
     return product.save();
   },
 
-  findProducts: async function ({ code, where, options, paginable = true }) {
+  findProducts: async function ({ code, where, options, paginable = false }) {
     let currentWhereQuery = {};
     let isPaginate = paginable;
+    let products;
+
     if (where) {
       currentWhereQuery = where;
     }
@@ -50,44 +40,53 @@ module.exports = {
       throw new Error('Geçersiz Sayfa Numarası');
     }
 
-    const queryBuilder = await ProductModel.find(query);
+    let queryBuilder = ProductModel.find(query);
 
     if (isPaginate) {
-      products = await queryBuilder
+      queryBuilder = await queryBuilder
         .skip((currentPage - 1) * perPage)
         .limit(perPage);
     }
 
-    const products = await queryBuilder.exec();
+    products = queryBuilder;
 
     if (!products) {
       throw new Error('Ürün bulunamadı');
       return;
     }
 
-    return products.length > 1 ? products : product[0];
+    return products.length > 1 ? products : products[0];
+  },
+  displayProductNames: async function ({ code }) {
+    const products = await ProductModel.aggregate([
+      { $match: { $and: [{ code, isActive: true }] } },
+      { $group: { _id: '$name' } },
+      { $project: { _id: 0, productName: '$_id' } },
+    ]);
+    return products.map((prd) => prd.productName);
   },
 
-  displayProductNames: async function ({ code, name }) {
-    console.log(code, name);
-    const product = await ProductModel.findOne({ $and: [{ code }, { name }] });
-    return product;
-  },
-
-  getCountProductAsCodeAndName: async function ({ code }) {
-    const countProduct = await ProductModel.countDocuments({ code });
+  getCountProductAsCodeAndName: async function ({ code, name }) {
+    const countProduct = await ProductModel.countDocuments({
+      $and: [{ code }, { name }],
+      isActive: true,
+    });
     return countProduct;
   },
 
-  update: async function ({ code, id, props }) {
+  update: async function ({ id, props }) {
     const product = await this.findProducts({
-      code,
       where: { property: _id, propResult: id },
     });
 
-    Object.assign(product, props);
+    const { code, ...rest } = props;
+
+    Object.assign(product, rest);
 
     await product.save();
   },
-  delete: async function () {},
+  delete: async function ({ id }) {
+    const product = await ProductModel.findByIdAndDelete(id);
+    return product;
+  },
 };
