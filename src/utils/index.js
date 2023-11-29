b8528eb11be4080f6b0468f5d551db57af9ffdbe
resat <resatyavcin@icloud.com>
2023-11-29 16:48:30 +0300

    Booking Feature Added

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 function responseJSON(status, message) {
   return {
     status,
@@ -11,7 +13,41 @@ const ProductCodeEnum = Object.freeze({
   ENGAGEMENT: 'EG',
 });
 
+const EventTypeEnum = Object.freeze({
+  WEDDING: 'WD',
+  HENNA: 'HN',
+  ENGAGEMENT: 'EG',
+});
+
+function checkDateOrder(dates) {
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < dates.length; i++) {
+      let currentMoment;
+      let nextMoment;
+      if (dates[i]) {
+        currentMoment = moment(dates[i]);
+        nextMoment = moment(dates[i + 1]);
+      }
+
+      if (currentMoment && nextMoment) {
+        if (currentMoment.isSameOrAfter(nextMoment)) {
+          throw new Error('Tarihler olay akış sırasına uygun değil.');
+        }
+      }
+    }
+
+    resolve(null);
+  });
+}
+
+function formatDate(date) {
+  moment(date).format('YYYY-MM-DDTHH:mm:ss.SSS[Z]');
+}
+
 module.exports = {
   responseJSON,
   ProductCodeEnum,
+  checkDateOrder,
+  EventTypeEnum,
+  formatDate,
 };
